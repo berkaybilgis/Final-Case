083545a8f155020b17e8b4f2b17fcc238374ca4f
berkaybilgis <berkaybilgis8@gmail.com>
2023-04-24 12:50:20 +0300

    header duzenlendi

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Heading } from "@chakra-ui/react";
 
 function Header() {
-  return <div>Header</div>;
+  return <Heading size="4xl">STAR WARS</Heading>;
 }
 
 export default Header;
