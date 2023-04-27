2ab8371749c080198ea3d6374ecaa6dd6c26ec10
berkaybilgis <berkaybilgis8@gmail.com>
2023-04-27 15:38:13 +0300

    filter duzenlendi

--- components/Filter.js
+++ components/Filter.js
@@ -5,27 +5,68 @@ import {
   FormLabel,
   FormControl,
   Button,
+  Alert,
+  AlertIcon,
+  Box,
 } from "@chakra-ui/react";
+import { useFilter } from "@/context/FilterContext";
 
 function Filter() {
+  const [search, setSearch] = useState("");
+  const [alert, setAlert] = useState("");
+  const { setFilter } = useFilter();
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedSearch = search.trim();
+    trimmedSearch
+      ? (setFilter(trimmedSearch), setAlert(""))
+      : setAlert("error");
+
+    setSearch("");
+  };
+
+  // console.log("filter input:", filtered);
   return (
     <div>
-      <FormControl>
-        <InputGroup mt="10" justifyContent="center">
-          <FormLabel mt="2">Name / Modal</FormLabel>
-          <Input type="text" placeholder="Name / Model" width="sm" />
-          <Button
-            colorScheme="blue"
-            ml="3"
-            pl="7"
-            pr="7"
-            size="md"
-            borderRadius="10"
-          >
-            Filter
-          </Button>
-        </InputGroup>
-      </FormControl>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <FormControl>
+          <InputGroup mt="10" justifyContent="center">
+            <FormLabel mt="2">Name / Modal</FormLabel>
+            <Input
+              type="text"
+              placeholder="Name / Model"
+              value={search}
+              width="sm"
+              onChange={(e) => handleChange(e)}
+            />
+
+            <Button
+              type="submit"
+              colorScheme="blue"
+              ml="3"
+              pl="7"
+              pr="7"
+              size="md"
+              borderRadius="10"
+            >
+              Filter
+            </Button>
+          </InputGroup>
+          {alert ? (
+            <Box textAlign="center" mt={2} maxWidth={330} mx="auto">
+              <Alert status={alert} borderRadius={7}>
+                <AlertIcon />
+                Please type what you want to search!
+              </Alert>
+            </Box>
+          ) : null}
+        </FormControl>
+      </form>
     </div>
   );
 }
