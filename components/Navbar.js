7091fe836be30be629804c6e8ae5a3356676b081
berkaybilgis <berkaybilgis8@gmail.com>
2023-04-28 14:13:17 +0300

    navbar degistirildi

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { Button, Box } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useFilter } from "@/context/FilterContext";
 
 function Navbar() {
   const router = useRouter();
-  const { setFilteredShips, setFilter } = useFilter();
+  const { setFilter } = useFilter();
 
   return (
-    <Box backgroundColor="gray.800" height={53} textAlign="left">
-      <Button
-        colorScheme="gray.800"
-        mt={1}
-        pl={10}
+    <>
+      <Box backgroundColor="gray.600" height={53} opacity="0.6"></Box>
+      <Box
+        ml={5}
+        className="home"
         onClick={() => {
           router.push("/");
           setFilter("");
         }}
-      >
-        Home
-      </Button>
-    </Box>
+      ></Box>
+    </>
   );
 }
 
